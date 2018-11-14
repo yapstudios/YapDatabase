1bb2f425867327b977ee3c4a9d29725fbe6a5147
Robbie Hanson <robbiehanson@deusty.com>
2018-11-14 16:59:22 -0500

    Improving documentation.

--- YapDatabase/Extensions/CloudCore/Utilities/Execution/YapDatabaseCloudCorePipeline.h
+++ YapDatabase/Extensions/CloudCore/Utilities/Execution/YapDatabaseCloudCorePipeline.h
@@ -11,8 +11,51 @@
 
 typedef NS_ENUM(NSInteger, YDBCloudCorePipelineAlgorithm) {
 	
+	/**
+	 * This is the default algorithm if you don't explicitly pick one.
+	 * It is HIGHLY recommended you start with this algorithm, until you become more advanced.
+	 *
+	 * The "Commit Graph" algorithm works as follows:
+	 *
+	 * - all operations added within a specific commit are added to their own "graph"
+	 * - the pipeline will execute each graph 1-at-a-time
+	 * - this ensures that graphs are completed in commit order
+	 *
+	 * That is, if a pipeline contains 2 graphs:
+	 * - graph "A" - representing operations from commit #32
+	 * - graph "B" - represending operations from commit #33
+	 *
+	 * Then the pipeline will ensure that ALL operations from graphA are either completed or skipped
+	 * before ANY operations from graphB start.
+	 *
+	 * This is the safest option because it means:
+	 * - you only have to think about operation dependencies within the context of a single commit
+	 * - the pipeline ensures the cloud moves from commit to commit (just as occurred locally)
+	**/
 	YDBCloudCorePipelineAlgorithm_CommitGraph = 0,
 	
+	/**
+	 * This is and ADVANCED algorithm that is only recommended after your cloud solution has matured.
+	 *
+	 * The "Flat Graph" algorithm works as follows:
+	 *
+	 * - all operations added within a specific commit are added to their own "graph"
+	 * - HOWEVER, the pipeline is free to start operations from ANY graph
+	 * - and it will do so, while respecting dependencies, priorities & maxConcurrentOperationCount
+	 *
+	 * In particular, what this means for you is:
+	 *
+	 * - you MUST create a FORMAL DEPENDENCY GRAPH (think: state diagram for dependencies)
+	 *
+	 * That is:
+	 * - given any possible operation (opA) in commitA
+	 * - and given any possible operation (opB) in commitB
+	 * - your formal dependency graph must determine if opB should depend on opA
+	 *
+	 * The recommended way of implementing your formal dependency graph is by
+	 * subclassing YapDatabaseCloudCoreTransaction & overriding the method
+	 * `processOperations:inPipeline:withGraphIdx:`.
+	**/
 	YDBCloudCorePipelineAlgorithm_FlatGraph = 1
 };
 
@@ -79,14 +122,7 @@ extern NSString *const YDBCloudCorePipelineActiveStatusChangedNotification;
  * specific pipeline). Within the graph are the various operations with their different dependencies & priorities.
  * The operations within a graph will be executed in accordance with the set dependencies & priorities.
  * 
- * The pipeline manages executing the operations within a graph.
- * It also ensures that graphs are completed in commit order.
- * 
- * That is, if a pipeline contains 2 graphs:
- * - graph "A" - representing operations from commit #32
- * - graph "B" - represending operations from commit #33
- * 
- * Then the pipeline will ensure that all operations from graphA complete before any operations from graphB start.
+ * The pipeline manages executing the operations within each graph.
 **/
 @interface YapDatabaseCloudCorePipeline : NSObject
 
